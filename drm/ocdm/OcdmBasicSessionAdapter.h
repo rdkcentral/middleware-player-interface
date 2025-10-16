811f69179e89bd9fb62803e4db1e9048cea117b1
rekhap2kandhavelan <rekhap2k@gmail.com>
2025-10-16 13:02:20 +0530

    Document constructor and destructor in OcdmBasicSessionAdapter
    Added constructor and destructor documentation for OCDMBasicSessionAdapter.

--- drm/ocdm/OcdmBasicSessionAdapter.h
+++ drm/ocdm/OcdmBasicSessionAdapter.h
@@ -31,10 +31,26 @@
 class OCDMBasicSessionAdapter : public OCDMSessionAdapter
 {
 public:
+	/**
+	* @brief Constructor for OCDMBasicSessionAdapter.
+	* @param drmHelper Shared pointer to a DrmHelper instance.
+	* @param drmCallbacks Pointer to a DrmCallbacks structure.
+	*/
 	OCDMBasicSessionAdapter(DrmHelperPtr drmHelper, DrmCallbacks *drmCallbacks)
 	: OCDMSessionAdapter(drmHelper, drmCallbacks)
 	{};
+	/**
+	* @brief Destructor for OCDMBasicSessionAdapter.
+	*/
 	~OCDMBasicSessionAdapter() {};
-
+	/**
+	* @brief Decrypt payload data using the provided IV.
+	* @param f_pbIV Pointer to the initialization vector (IV).
+	* @param f_cbIV Size of the IV in bytes.
+	* @param payloadData Pointer to the encrypted payload data.
+	* @param payloadDataSize Size of the encrypted payload data in bytes.
+	* @param ppOpaqueData Pointer to store the decrypted opaque data.
+	* @return Integer status code (e.g., 0 for success, non-zero for failure).
+	*/
 	int decrypt(const uint8_t *f_pbIV, uint32_t f_cbIV, const uint8_t *payloadData, uint32_t payloadDataSize, uint8_t **ppOpaqueData);
 };
