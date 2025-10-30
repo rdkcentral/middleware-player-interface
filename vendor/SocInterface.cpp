023a01998cf6ffd8d463cd7058ba72b5aa12997b
nrames759 <Naren_Ramesh@comcast.com>
2025-10-30 10:06:28 +0530

    build fix

--- vendor/SocInterface.cpp
+++ vendor/SocInterface.cpp
@@ -19,10 +19,14 @@
 
 #include <assert.h>
 #include "SocInterface.h"
+#include "vendor/default/DefaultSocInterface.h"
+
+#if !defined(__APPLE__) && !defined(UBUNTU)
 #include "vendor/amlogic/AmlogicSocInterface.h"
 #include "vendor/brcm/BrcmSocInterface.h"
 #include "vendor/realtek/RealtekSocInterface.h"
-#include "vendor/default/DefaultSocInterface.h"
+#endif
+
 
 /**
  * @brief Checks if the input string starts with the given prefix.
@@ -158,6 +162,7 @@ std::shared_ptr<SocInterface> SocInterface::CreateSocInterface()
 		{
 			platformType = InferPlatformFromPluginScan();
 		}
+#if !defined(__APPLE__) && !defined(UBUNTU)
 		switch (platformType)
 		{
 			case SOC_PLATFORM_AMLOGIC:
@@ -173,6 +178,9 @@ std::shared_ptr<SocInterface> SocInterface::CreateSocInterface()
 				socInterface = std::make_shared<DefaultSocInterface>();
 				break;
 		}
+#else
+		socInterface = std::make_shared<DefaultSocInterface>();
+#endif
 	}
 	return socInterface;
 }
