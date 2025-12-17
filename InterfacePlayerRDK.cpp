c8764fbea48bd3290c71bc8af3d235b97d49bd19
rekhap2kandhavelan <rekhap2k@gmail.com>
2025-12-17 15:38:51 +0530

    Update InterfacePlayerRDK.cpp

--- InterfacePlayerRDK.cpp
+++ InterfacePlayerRDK.cpp
@@ -2903,7 +2903,7 @@ bool InterfacePlayerRDK::HandleVideoBufferSent()
 
 void InterfacePlayerRDK::SetPlayerName(std::string name)
 {
-	interfacePlayerPriv->mPlayerName = name;
+	interfacePlayerPriv->mPlayerName = std::move(name);
 }
 
 /**
