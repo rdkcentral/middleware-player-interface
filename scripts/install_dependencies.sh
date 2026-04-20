9584281557ef167b48401436cd8bd1a048631d57
deepikasri <deepikasri_n@comcast.com>
2026-04-20 19:08:32 +0530

    resolved mac compiltion

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -18,7 +18,7 @@
 # limitations under the License.
 #
 
-declare DEFAULT_OPENSSL_VERSION="openssl@3.4"
+declare DEFAULT_OPENSSL_VERSION="openssl@3"
 
 function install_pkgs_pkgconfig_darwin_fn()
 {
@@ -199,7 +199,7 @@ function install_pkgs_fn()
           brew update
       fi
 
-      install_pkgs_darwin_fn git json-glib cmake "openssl@3.4" libxml2 ossp-uuid cjson gnu-sed jpeg-turbo taglib speex mpg123 meson ninja pkg-config flac asio jsoncpp lcov gcovr jq curl
+      install_pkgs_darwin_fn git json-glib cmake "openssl@3" libxml2 ossp-uuid cjson gnu-sed jpeg-turbo taglib speex mpg123 meson ninja pkg-config flac asio jsoncpp lcov gcovr jq curl
       install_pkgs_darwin_fn coreutils websocketpp "boost@1.85" jansson libxkbcommon cppunit gnu-sed fontconfig doxygen graphviz tinyxml2 openldap krb5
 
       # ORC causes compile errors on x86_64 Mac, but not on ARM64
