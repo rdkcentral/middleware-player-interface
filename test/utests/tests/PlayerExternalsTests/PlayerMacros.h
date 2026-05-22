502954718f4134c5ff7a298cfbc5c636b85aa740
dp0000 <53818367+dp0000@users.noreply.github.com>
2026-05-22 16:56:10 +0530

    Update PlayerMacros.h
    address the fossid scan issues

--- test/utests/tests/PlayerExternalsTests/PlayerMacros.h
+++ test/utests/tests/PlayerExternalsTests/PlayerMacros.h
@@ -1,21 +1,7 @@
 #pragma once
 #include <gtest/gtest.h>
 
-#define GTEST_TEST_NO_THROW_(statement, fail) \
-  GTEST_AMBIGUOUS_ELSE_BLOCKER_ \
-  if (::testing::internal::TrueWithString gtest_msg{}) { \
-    try { \
-      GTEST_SUPPRESS_UNREACHABLE_CODE_WARNING_BELOW_(statement); \
-    } \
-    GTEST_TEST_NO_THROW_CATCH_STD_EXCEPTION_() \
-    catch (...) { \
-      gtest_msg.value = "it throws."; \
-      goto GTEST_CONCAT_TOKEN_(gtest_label_testnothrow_, __LINE__); \
-    } \
-  } else \
-    GTEST_CONCAT_TOKEN_(gtest_label_testnothrow_, __LINE__): \
-      fail(("Expected: " #statement " doesn't throw an exception.\n" \
-            "  Actual: " + gtest_msg.value).c_str())
+
 
 #define MY_EXPECT_NO_THROW(statement)                 \
     do {                                              \
