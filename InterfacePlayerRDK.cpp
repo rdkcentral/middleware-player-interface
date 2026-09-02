e4091725d6de2732a1d9e176f62bc160ab42dcef
Deepikasri N <deepikasri_n@comcast.com>
2026-09-02 18:10:55 +0530

    added fix to check the frist frame timeout

--- InterfacePlayerRDK.cpp
+++ InterfacePlayerRDK.cpp
@@ -1461,7 +1461,7 @@ void InterfacePlayerRDK::TearDownStream(int type)
 	TuneCount1++;	
 
 	MW_LOG_MIL("TearDownStream count incremented %d", TuneCount1++);
-	if (TuneCount1 == 100)
+	if (TuneCount1 == 10)
 	{
 	   type =4;
 	}
