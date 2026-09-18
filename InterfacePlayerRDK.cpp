6d8d366e39a724c5de36c1a335c16b3a4b240908
dp0000 <53818367+dp0000@users.noreply.github.com>
2026-09-18 11:18:26 +0530

    Update InterfacePlayerRDK.cpp

--- InterfacePlayerRDK.cpp
+++ InterfacePlayerRDK.cpp
@@ -33,8 +33,7 @@
 #include "TextStyleAttributes.h"
 #include <memory>
 #include <gst/gst.h>
-#include "TelemetryMarkers.h"
-#include "PlayerTelemetry.h"
+
 #ifdef PLAYER_TELEMETRY_SUPPORT
 #include "PlayerTelemetry2.hpp"
 #endif //PLAYER_TELEMETRY_SUPPORT
