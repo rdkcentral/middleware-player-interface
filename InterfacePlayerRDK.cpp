8bdeba01e9c7f450dd249289d02b68eac3a45bff
Deepikasri N <deepikasri_n@comcast.com>
2026-09-21 08:15:23 +0530

    Added coverity fixes

--- InterfacePlayerRDK.cpp
+++ InterfacePlayerRDK.cpp
@@ -377,7 +377,7 @@ void InterfacePlayerRDK::ConfigurePipeline(int format, int audioFormat, int subF
 	mFirstFrameRequired = FirstFrameFlag;
 
 	/* First-frame watchdog: tune not completed if callback never arrives */
-	if (mFirstFrameRequired && interfacePlayerPriv && interfacePlayerPriv->gstPrivateContext)
+	if (mFirstFrameRequired)
 	{
 		/* reset for new tune attempt */
 		interfacePlayerPriv->gstPrivateContext->firstFrameReceived = false;
