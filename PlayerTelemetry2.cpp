257b757dab855c0741787111a7001b6994f8f286
Deepikasri N <deepikasri_n@comcast.com>
2026-09-24 22:00:27 +0530

    Revert "Potential fix for pull request finding"
    This reverts commit 09f42d0db86498af642d83d7f39d01cace427386.

--- PlayerTelemetry2.cpp
+++ PlayerTelemetry2.cpp
@@ -131,11 +131,7 @@ bool PlayerTelemetry2::send( const std::string &markerName, const char *  data)
 }
 void PlayerTelemetry2::sendEvent(const std::string& eventName)
 {
-    if (!mInitializer.isInitialized())
-    {
-        mInitializer.Init();
-    }
-
+    bool init = mInitializer.isInitialized();
     MW_LOG_MIL("[TELEMETRY] event=%s", eventName.c_str());
     t2_event_d(const_cast<char*>(eventName.c_str()), 1);
 }
